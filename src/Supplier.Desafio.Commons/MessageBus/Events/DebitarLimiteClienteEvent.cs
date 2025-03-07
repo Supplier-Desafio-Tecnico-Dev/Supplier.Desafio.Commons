2f9fd40f6e219fb1b2f30d7cfb09c1126cdbaeca
Igor Daflon <igordafloncouto@gmail.com>
2025-03-06 23:08:23 -0300

    Add ResultadoTransacaoEvent

--- src/Supplier.Desafio.Commons/MessageBus/Events/DebitarLimiteClienteEvent.cs
+++ src/Supplier.Desafio.Commons/MessageBus/Events/DebitarLimiteClienteEvent.cs
@@ -2,6 +2,7 @@ namespace Supplier.Desafio.Commons.MessageBus.Events;
 
 public class DebitarLimiteClienteEvent
 {
+    public int IdTransacao { get; set; }
     public int IdCliente { get; set; }
     public decimal ValorADebitar { get; set; }
 }
---
+++ src/Supplier.Desafio.Commons/MessageBus/Events/ResultadoTransacaoEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Supplier.Desafio.Commons.MessageBus.Events;
+
+public class ResultadoTransacaoEvent
+{
+    public int IdTransacao { get; set; }
+    public string StatusTransacao { get; set; }
+}
